5a5d6600a94af8bd4163144820b04e2dd795051c
dhanushkiren <dhanushrameshkvpt03@gmail.com>
2025-01-01 17:48:28 +0530

    modified the route name to smaller case

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import TabNavigator from "./src/Navigation/TabNavigator.js"; // Import TabNavigator
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import PaymentPage from "./src/screens/PaymentPage.js";
@@ -17,18 +16,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login"
+        initialRouteName="home"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="searchlist" component={searchlist} />
-        <Stack.Screen name="FAQ" component={FAQScreen} />
-        <Stack.Screen name="TabHome" component={Home} />
-        <Stack.Screen name="Payment" component={PaymentPage} />
-        <Stack.Screen name="Product" component={ProductPage} />
-        <Stack.Screen name="Profile" component={EditProfile} />
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
-        {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
+        <Stack.Screen name="faq" component={FAQScreen} />
+        <Stack.Screen name="home" component={Home} />
+        <Stack.Screen name="payment" component={PaymentPage} />
+        <Stack.Screen name="product" component={ProductPage} />
+        <Stack.Screen name="profile" component={EditProfile} />
+        <Stack.Screen name="login" component={LoginScreen} />
+        <Stack.Screen name="register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
