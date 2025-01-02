566bd9a15cd65399a35babe0a433fccd7a7e73d2
PetchiSubetchica <subetchicabalu@gmail.com>
2025-01-02 23:31:18 +0530

    Add reusable MenuBar component

---
+++ src/components/MenuBar.js
@@ -0,0 +1,93 @@
+// src/components/MenuBar.js
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from '../screens/Home';
+import { View } from 'react-native'; // Placeholder for other tabs
+
+const Tab = createBottomTabNavigator();
+
+// Placeholder Component to display the plain color screen for each and every tab used as the icons in our Home Page
+const PlaceholderScreen = () => {
+  return <View style={{ flex: 1, backgroundColor: '#8D67F1' }} />;
+};
+
+const MenuBar = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarStyle: {
+          backgroundColor: '#8D67F1',
+          paddingBottom: 5,
+          height: 60,
+        },
+        tabBarActiveTintColor: '#ffffff',
+        tabBarInactiveTintColor: '#dcdcdc',
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Profile') {
+            iconName = 'account';
+          } else if (route.name === 'Cart') {
+            iconName = 'cart';
+          } else if (route.name === 'Wallet') {
+            iconName = 'wallet';
+          } else if (route.name === 'Menu') {
+            iconName = 'menu';
+          }
+
+          return <MaterialCommunityIcons name={iconName} color={color} size={size} />;
+        },
+        tabBarLabelStyle: {
+          fontSize: 12,
+          fontWeight: '600',
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={PlaceholderScreen} 
+        options={{
+          tabBarLabel: 'Profile',
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={PlaceholderScreen} 
+        options={{
+          tabBarLabel: 'Cart',
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Wallet"
+        component={PlaceholderScreen} 
+        options={{
+          tabBarLabel: 'Wallet',
+          headerShown: false,
+        }}
+      />
+      <Tab.Screen
+        name="Menu"
+        component={PlaceholderScreen} 
+        options={{
+          tabBarLabel: 'Menu',
+          headerShown: false,
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default MenuBar;
