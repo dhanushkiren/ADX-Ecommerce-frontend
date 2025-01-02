9b3e8d4491003ebdfb4fb02df9f551abb4df8b7a
Karthika-Devi-M <devikarthika821@gmail.com>
2025-01-02 22:30:00 +0530

    Order splashscreen completed

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import EditProfile  from "./src/components/EditProfile";
-
+import SplashscreenLogo from "./src/screens/SplashscreenLogo"; 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const[setIsShowSplash] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsShowSplash(false);
+    },4000);
+  });
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Profile">
         <Stack.Screen name="Profile" component={EditProfile} />
+        
         {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/screens/OrderSplashscreen.js
@@ -0,0 +1,29 @@
+import {Image , StyleSheet ,View } from "react-native";
+import Icon  from "./assets/carticon.png"
+import { withLayoutContext } from "expo-router";
+export default function SplashScreen()
+{
+return
+(
+ <View style={StyleSheet.container}>
+    <View>
+       <Image source={Icon} style={StyleSheet.images}/>
+    </View>
+ </View>
+)
+}
+
+const styles =StyleSheet.create({
+   container:{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+      backgroundColor:"#00008B", 
+   },
+   image:
+   {
+     width:100,
+     height:100,
+     resizeMode:"cover" 
+   }
+})
