37fe0ea7683bb8436b6f362d9ca0366858c44761
Renuka-6 <renukasubramanian723@gmail.com>
2025-01-08 14:32:33 +0530

    updated cart page

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,3 +1,8 @@
 export const apiConfig = {
   postLogin: `auth/login`,
+  addToCart: (userId) => `cart/${userId}`,
+  viewCart: (userId) => `cart/${userId}`,
+  deleteCartItem: (userId, itemId) => `cart/${userId}/${itemId}`,
+  clearCart: `cart/clear`,
 };
+
