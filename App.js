0219c61341443a0371a0692807c2b38bc4c520e4
HARI VISHVANATH S <harivishvanath7@gmail.com>
2025-01-23 17:22:11 +0530

    Latest Changes

--- App.js
+++ App.js
@@ -58,7 +58,7 @@ function MyStack() {
     <NavigationContainer>
       <Stack.Navigator
         // initialRouteName={token ? "home" : "login"}
-        initialRouteName="history"
+        initialRouteName="menu"
         screenOptions={{ headerShown: false }}
       >
         {/* Define your screens */}
