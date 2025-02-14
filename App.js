48d77a2c725196de3c54c5207d58121237515b83
HARI VISHVANATH S <harivishvanath7@gmail.com>
2025-02-14 10:30:00 +0530

    Branch Created for Dynamic Fetching of product details

--- App.js
+++ App.js
@@ -77,8 +77,8 @@ function MyStack() {
   return (  
     <>  
       <Stack.Navigator
-        initialRouteName={token ? "home" : "login"}
-        // initialRouteName="product"
+        // initialRouteName={token ? "home" : "login"}
+        initialRouteName="home"
         screenOptions={{ headerShown: false }}
       >
         {/* Define your screens */}
