cb391b336b54ca09641a15cf134c2780925f99f0
HARI VISHVANATH S <harivishvanath7@gmail.com>
2025-02-17 23:41:18 +0530

    Latest changes

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -49,20 +49,21 @@ const Home = ({ navigation }) => {
   };
 
   const renderProductItem = ({ item }) => (
-    <View style={styles.productCardContainer}>
+    <TouchableOpacity
+      style={styles.productCardContainer}
+      onPress={() => navigation.navigate("product", { product: item })}
+    >
       <Card style={styles.productCard}>
-
         <Card.Cover
           style={styles.productImage}
-          source={{ uri: item.imageUrl }}
+          source={{ uri: item.imageUrl || 'https://via.placeholder.com/150' }}
         />
         <Card.Content>
           <Text style={styles.productTitle}>{item.name}</Text>
           <Text style={styles.productDescription}>{item.description}</Text>
-
         </Card.Content>
       </Card>
-    </View>
+    </TouchableOpacity>
   );
 
   const renderProducts = () => {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL =  "http://192.168.101.8:8080/api/"
+export const API_BASE_URL =  "http://192.168.101.5:8080/api/"
